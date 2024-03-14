016e76932526a730020e4f1bccd576fcbf98affe
Stephan Pauxberger <stephan@blackbuild.com>
2024-03-14 16:54:29 +0100

    Initial setup

---
+++ anno-docimal-generator/src/main/java/com/blackbuild/annodocimal/generator/SpecConverter.java
@@ -0,0 +1,133 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2024 Stephan Pauxberger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.blackbuild.annodocimal.generator;
+
+import com.blackbuild.annodocimal.annotations.Javadocs;
+import com.squareup.javapoet.*;
+
+import javax.lang.model.element.Modifier;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.EnumSet;
+
+/**
+ * Converts real reflection elements to their JavaPoet Spec counterparts.
+ */
+public class SpecConverter {
+
+    private SpecConverter() {
+        // static only
+    }
+
+    public static JavaFile toJavaFile(Class<?> type) {
+        return JavaFile.builder(type.getPackage().getName(), toTypeSpec(type)).build();
+    }
+
+    public static TypeSpec toTypeSpec(Class<?> type) {
+        TypeSpec.Builder builder = TypeSpec.classBuilder(type.getSimpleName())
+                .superclass(type.getSuperclass())
+                .addModifiers(getModifiers(type.getModifiers()));
+
+        for (Class<?> aClass : type.getInterfaces())
+            builder.addSuperinterface(aClass);
+
+        for (Annotation annotation : type.getAnnotations())
+            if (annotation instanceof Javadocs)
+                builder.addJavadoc(((Javadocs) annotation).value());
+            else
+                builder.addAnnotation(toAnnotationSpec(annotation));
+
+        for (Method method : type.getDeclaredMethods()) {
+            if (shouldIgnore(method)) continue;
+            builder.addMethod(toMethodSpec(method));
+        }
+
+        for (Field field : type.getDeclaredFields()) {
+            if (shouldIgnore(field)) continue;
+            builder.addField(toFieldSpec(field));
+        }
+
+        return builder.build();
+    }
+
+    private static boolean shouldIgnore(Member member) {
+        return member.isSynthetic() || (member.getModifiers() & 1) == 0 || member.getName().contains("$");
+    }
+
+    public static FieldSpec toFieldSpec(Field field) {
+        FieldSpec.Builder builder = FieldSpec.builder(field.getType(), field.getName(), getModifiers(field.getModifiers()));
+
+        for (Annotation annotation : field.getAnnotations())
+            if (annotation instanceof Javadocs)
+                builder.addJavadoc(((Javadocs) annotation).value());
+            else
+                builder.addAnnotation(toAnnotationSpec(annotation));
+
+        return builder.build();
+    }
+
+    public static MethodSpec toMethodSpec(Method method) {
+        MethodSpec.Builder builder = MethodSpec.methodBuilder(method.getName())
+                .addModifiers(getModifiers(method.getModifiers()))
+                .returns(method.getReturnType());
+
+        for (Annotation annotation : method.getAnnotations())
+            if (annotation instanceof Javadocs)
+                builder.addJavadoc(((Javadocs) annotation).value());
+            else
+                builder.addAnnotation(toAnnotationSpec(annotation));
+
+        for (Parameter param : method.getParameters())
+            builder.addParameter(param.getType(), param.getName(), SpecConverter.getModifiers(param.getModifiers()));
+
+        return builder.build();
+    }
+
+    public static ParameterSpec toParameterSpec(Parameter parameter) {
+        ParameterSpec.Builder builder = ParameterSpec.builder(parameter.getType(), parameter.getName(), getModifiers(parameter.getModifiers()));
+        if (parameter.isAnnotationPresent(Javadocs.class))
+            builder.addJavadoc(parameter.getAnnotation(Javadocs.class).value());
+        for (Annotation annotation : parameter.getAnnotations())
+            builder.addAnnotation(toAnnotationSpec(annotation));
+        return builder.build();
+    }
+
+    public static AnnotationSpec toAnnotationSpec(Annotation annotation) {
+        return AnnotationSpec.get(annotation);
+    }
+
+    static Modifier[] getModifiers(int mod) {
+        EnumSet<Modifier> result = EnumSet.noneOf(Modifier.class);
+
+        if ((mod & 0x0001) != 0) result.add(Modifier.PUBLIC);
+        if ((mod & 0x0002) != 0) result.add(Modifier.PRIVATE);
+        if ((mod & 0x0004) != 0) result.add(Modifier.PROTECTED);
+        if ((mod & 0x0008) != 0) result.add(Modifier.STATIC);
+
+        return result.toArray(new Modifier[0]);
+    }
+}
---
+++ anno-docimal-generator/src/test/groovy/com/blackbuild/annodocimal/generator/AnnoDocGeneratorTest.groovy
@@ -0,0 +1,96 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2024 Stephan Pauxberger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.blackbuild.annodocimal.generator
+
+class AnnoDocGeneratorTest extends ClassGeneratingTest {
+
+    def "basic conversion"() {
+        given:
+        def clazz = createClass("""
+            package dummy
+            class TestClass {
+                String field
+                void method() {
+                    println "Hello"
+                }
+            }
+        """)
+        StringBuilder output = new StringBuilder()
+
+        when:
+        AnnoDocGenerator.generate(clazz, output)
+        def source = output.toString().readLines()
+        def trimmed = source*.trim()
+
+        then:
+        noExceptionThrown()
+        source.contains("package dummy;")
+        source.find { it.startsWith("public class TestClass ") }
+        source.find { it.contains("implements GroovyObject") }
+        trimmed.contains( "public void method() {" )
+    }
+
+    def "basic test with generated documentation"() {
+        given:
+        def clazz = createClass("""
+            package dummy
+     
+            import com.blackbuild.annodocimal.annotations.Javadocs
+            
+            @Javadocs("This is a test class")
+            class TestClass {
+                @Javadocs("This is a field")
+                String field
+                
+                @Javadocs("This is a method")
+                void method() {
+                    println "Hello"
+                }
+            }
+        """)
+        StringBuilder output = new StringBuilder()
+
+        when:
+        AnnoDocGenerator.generate(clazz, output)
+
+        def sourceText = output.toString()
+        def source = sourceText.readLines()
+        def trimmed = source*.trim()
+
+        then:
+        noExceptionThrown()
+        source.contains("package dummy;")
+        sourceText.contains '''/**
+ * This is a test class
+ */
+public class TestClass'''
+
+        source.find { it.startsWith("public class TestClass ") }
+        source.find { it.contains("implements GroovyObject") }
+        trimmed.contains( "public void method() {" )
+    }
+
+
+
+}
